1579b070107c4ec65a9e42633ac34ebc7fb974aa
一坨憨盐 <2246983324@qq.com>
2026-09-23 23:15:55 +0800

    chore: 移除无关练习文件，仅保留项目核心代码

--- src/OOPDemo.java
+++
@@ -1,9 +0,0 @@
-public class OOPDemo {
-    public static void main(String[] args) {
-
-        Student stu5 = new Student("王小刚", 20, "20240901");
-        stu5.introduce();
-        Student stu6 = new Student("赵小静", 19);
-        stu6.introduce();
-    }
-}
