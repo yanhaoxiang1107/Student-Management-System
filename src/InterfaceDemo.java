c1429ee6022e536ad9ed168854cabbc2d4ee356d
一坨憨盐 <2246983324@qq.com>
2026-09-23 23:23:32 +0800

    chore: 移除多余的接口练习文件

--- src/InterfaceDemo.java
+++
@@ -1,8 +0,0 @@
-public class InterfaceDemo {
-    public static void main(String[] args) {
-        Dog dog = new Dog("旺财");
-        dog.eat();
-        dog.makeSound();
-        dog.swim();
-    }
-}
